837ec8d90f0a849d1b0e7962fafcf1b9bb4ef69d
Sam Bryant <srbry@hotmail.com>
2016-03-24 17:39:46 +0000

    improve test coverage of processor/utils

--- processor/utils/utility_test.go
+++ processor/utils/utility_test.go
@@ -52,6 +52,13 @@ var _ = Describe("#IsAppHealthy", func() {
 })
 
 var _ = Describe("#PickAppInstance", func() {
+	Context("When there are no app instances", func() {
+		It("Returns an app index of 0", func() {
+			appInstances := make(map[string]cfclient.AppInstance)
+			Expect(utils.PickAppInstance(appInstances)).To(Equal(0))
+		})
+	})
+
 	Context("When there is 1 app instance", func() {
 		It("Returns an app index of 0", func() {
 			appInstances := make(map[string]cfclient.AppInstance)
@@ -94,34 +101,76 @@ var _ = Describe("#PickAppInstance", func() {
 })
 
 var _ = Describe("#GetBoundApps", func() {
-	It("Gets all bound apps that require processing", func() {
-		db, mock, err := sqlmock.New()
-		if err != nil {
-			fmt.Printf("\nan error '%s' was not expected when opening a stub database connection\n", err)
-			os.Exit(1)
-		}
-		defer db.Close()
+	Context("when services instances can be fetched", func() {
+		Context("and service bindings can be fetched", func() {
+			It("Gets all bound apps that require processing", func() {
+				db, mock, err := sqlmock.New()
+				if err != nil {
+					fmt.Printf("\nan error '%s' was not expected when opening a stub database connection\n", err)
+					os.Exit(1)
+				}
+				defer db.Close()
 
-		instanceRows := sqlmock.NewRows([]string{"id", "dashboardURL", "planID", "probability", "frequency"}).
-			AddRow("1", "example.com/1", "1", 0.2, 5).
-			AddRow("2", "example.com/2", "1", 0.4, 10).
-			AddRow("3", "example.com/3", "1", 0, 10)
+				instanceRows := sqlmock.NewRows([]string{"id", "dashboardURL", "planID", "probability", "frequency"}).
+					AddRow("1", "example.com/1", "1", 0.2, 5).
+					AddRow("2", "example.com/2", "1", 0.4, 10).
+					AddRow("3", "example.com/3", "1", 0, 10)
 
-		bindingRows := sqlmock.NewRows([]string{"id", "appID", "servicePlanID", "serviceInstanceID", "lastProcessed"}).
-			AddRow("1", "1", "1", "1", "2014-11-12T10:31:20Z").
-			AddRow("2", "2", "1", "2", "2014-11-12T10:34:20Z").
-			AddRow("3", "3", "1", "2", "").
-			AddRow("4", "4", "1", "3", "2014-11-12T10:34:20Z").
-			AddRow("5", "5", "1", "4", "2014-11-12T10:34:20Z")
+				bindingRows := sqlmock.NewRows([]string{"id", "appID", "servicePlanID", "serviceInstanceID", "lastProcessed"}).
+					AddRow("1", "1", "1", "1", "2014-11-12T10:31:20Z").
+					AddRow("2", "2", "1", "2", "2014-11-12T10:34:20Z").
+					AddRow("3", "3", "1", "2", "").
+					AddRow("4", "4", "1", "3", "2014-11-12T10:34:20Z").
+					AddRow("5", "5", "1", "4", "2014-11-12T10:34:20Z")
 
-		mock.ExpectQuery("^SELECT (.+) FROM service_instances$").WillReturnRows(instanceRows)
-		mock.ExpectQuery("^SELECT (.+) FROM service_bindings$").WillReturnRows(bindingRows)
+				mock.ExpectQuery("^SELECT (.+) FROM service_instances$").WillReturnRows(instanceRows)
+				mock.ExpectQuery("^SELECT (.+) FROM service_bindings$").WillReturnRows(bindingRows)
 
-		services := utils.GetBoundApps(db)
-		Expect(services).To(HaveLen(3))
-		Expect(services).To(ContainElement(model.Service{AppID: "1", LastProcessed: "2014-11-12T10:31:20Z", Probability: 0.2, Frequency: 5}))
-		Expect(services).To(ContainElement(model.Service{AppID: "2", LastProcessed: "2014-11-12T10:34:20Z", Probability: 0.4, Frequency: 10}))
-		Expect(services).To(ContainElement(model.Service{AppID: "3", LastProcessed: "", Probability: 0.4, Frequency: 10}))
+				services := utils.GetBoundApps(db)
+				Expect(services).To(HaveLen(3))
+				Expect(services).To(ContainElement(model.Service{AppID: "1", LastProcessed: "2014-11-12T10:31:20Z", Probability: 0.2, Frequency: 5}))
+				Expect(services).To(ContainElement(model.Service{AppID: "2", LastProcessed: "2014-11-12T10:34:20Z", Probability: 0.4, Frequency: 10}))
+				Expect(services).To(ContainElement(model.Service{AppID: "3", LastProcessed: "", Probability: 0.4, Frequency: 10}))
+			})
+		})
+
+		Context("and service bindings cannot be fetched", func() {
+			It("returns an empty services object", func() {
+				db, mock, err := sqlmock.New()
+				if err != nil {
+					fmt.Printf("\nan error '%s' was not expected when opening a stub database connection\n", err)
+					os.Exit(1)
+				}
+				defer db.Close()
+
+				instanceRows := sqlmock.NewRows([]string{"id", "dashboardURL", "planID", "probability", "frequency"}).
+					AddRow("1", "example.com/1", "1", 0.2, 5).
+					AddRow("2", "example.com/2", "1", 0.4, 10).
+					AddRow("3", "example.com/3", "1", 0, 10)
+
+				mock.ExpectQuery("^SELECT (.+) FROM service_instances$").WillReturnRows(instanceRows)
+				mock.ExpectQuery("^SELECT (.+) FROM service_bindings$").WillReturnError(fmt.Errorf("An error has occurred: %s", "DB error"))
+
+				services := utils.GetBoundApps(db)
+				Expect(services).To(HaveLen(0))
+			})
+		})
+	})
+
+	Context("when services instances cannot be fetched", func() {
+		It("returns an empty services object", func() {
+			db, mock, err := sqlmock.New()
+			if err != nil {
+				fmt.Printf("\nan error '%s' was not expected when opening a stub database connection\n", err)
+				os.Exit(1)
+			}
+			defer db.Close()
+
+			mock.ExpectQuery("^SELECT (.+) FROM service_instances$").WillReturnError(fmt.Errorf("An error has occurred: %s", "DB error"))
+
+			services := utils.GetBoundApps(db)
+			Expect(services).To(HaveLen(0))
+		})
 	})
 })
 
@@ -176,6 +225,25 @@ var _ = Describe("#UpdateLastProcessed", func() {
 		mock.ExpectExec("UPDATE service_bindings.*").WithArgs(lastProcessed, appID).WillReturnResult(sqlmock.NewResult(1, 1))
 		Expect(utils.UpdateLastProcessed(db, appID, lastProcessed)).To(BeNil())
 	})
+
+	Context("when the sql update command errors", func() {
+		It("returns an error", func() {
+			db, mock, err := sqlmock.New()
+			if err != nil {
+				fmt.Printf("\nan error '%s' was not expected when opening a stub database connection\n", err)
+				os.Exit(1)
+			}
+			defer db.Close()
+
+			appID := "1"
+			lastProcessed := "2014-11-12T10:34:20Z"
+
+			mock.ExpectExec("UPDATE service_bindings.*").WithArgs(lastProcessed, appID).WillReturnError(fmt.Errorf("An error has occurred: %s", "UPDATE error"))
+			err = utils.UpdateLastProcessed(db, appID, lastProcessed)
+			Expect(err).ToNot(BeNil())
+			Expect(err.Error()).To(Equal("An error has occurred: UPDATE error"))
+		})
+	})
 })
 
 var _ = Describe("#TimeNow", func() {
@@ -194,8 +262,18 @@ var _ = Describe("#TimeNow", func() {
 var _ = Describe("LoadCFConfig", func() {
 	var vcapServicesJSON string
 
-	BeforeEach(func() {
-		vcapServicesJSON = `{
+	JustBeforeEach(func() {
+		os.Setenv("VCAP_SERVICES", vcapServicesJSON)
+	})
+
+	AfterEach(func() {
+		os.Unsetenv("VCAP_SERVICES")
+	})
+
+	Context("When VCAP Services is valid json", func() {
+		Context("and the cf-service exists", func() {
+			BeforeEach(func() {
+				vcapServicesJSON = `{
   "user-provided": [
    {
     "credentials": {
@@ -211,19 +289,50 @@ var _ = Describe("LoadCFConfig", func() {
    }
   ]
  }`
-		os.Setenv("VCAP_SERVICES", vcapServicesJSON)
+			})
+
+			It("Returns *cfclient.Config", func() {
+				config := utils.LoadCFConfig()
+				Expect(config).To(Equal(&cfclient.Config{ApiAddress: "https://api.example.com",
+					LoginAddress:      "https://login.example.com",
+					Username:          "test_user",
+					Password:          "test_password",
+					SkipSslValidation: true}))
+			})
+		})
+
+		Context("and the cf-service does not exist", func() {
+			BeforeEach(func() {
+				vcapServicesJSON = `{}`
+			})
+
+			It("returns an empty config object", func() {
+				config := utils.LoadCFConfig()
+				Expect(config).To(Equal(&cfclient.Config{}))
+			})
+		})
 	})
 
-	AfterEach(func() {
-		os.Unsetenv("VCAP_SERVICES")
-	})
+	Context("When VCAP Services is invalid json", func() {
+		BeforeEach(func() {
+			vcapServicesJSON = `{
+  "user-provided": [
+   {
+    "credentials": {
+    	"username":"test_user",
+    	"password":"test_password",
+    	"domain":"example.com",
+    	"skipsslvalidation
+    "syslog_drain_url": "",
+    "tags": []
+   }
+  ]
+ }`
+		})
 
-	It("Returns *cfclient.Config", func() {
-		config := utils.LoadCFConfig()
-		Expect(config).To(Equal(&cfclient.Config{ApiAddress: "https://api.example.com",
-			LoginAddress:      "https://login.example.com",
-			Username:          "test_user",
-			Password:          "test_password",
-			SkipSslValidation: true}))
+		It("returns an empty config object", func() {
+			config := utils.LoadCFConfig()
+			Expect(config).To(Equal(&cfclient.Config{}))
+		})
 	})
 })
