2b77bc3624dbcfc1247d9e91ee93a9344a2fe9e4
Sam Bryant <srbry@hotmail.com>
2016-03-24 10:44:29 +0000

    improve broker/config test coverage

--- broker/config/config_test.go
+++ broker/config/config_test.go
@@ -18,4 +18,20 @@ var _ = Describe("#LoadConfig_#GetConfig", func() {
 		Expect(conf.DefaultProbability).To(Equal(0.4))
 		Expect(conf.DefaultFrequency).To(Equal(10))
 	})
+
+	Context("When the file cannot be read", func() {
+		It("returns an error", func() {
+			_, err := LoadConfig("fixtures/no_config.json")
+			Expect(err).ToNot(BeNil())
+			Expect(err.Error()).To(Equal("open fixtures/no_config.json: no such file or directory"))
+		})
+	})
+
+	Context("When the file is invalid json", func() {
+		It("returns an error", func() {
+			_, err := LoadConfig("fixtures/invalid_config.json")
+			Expect(err).ToNot(BeNil())
+			Expect(err.Error()).To(MatchRegexp("invalid"))
+		})
+	})
 })
